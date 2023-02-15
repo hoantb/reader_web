50e9f696dfceadeb019e7732bb53e22e64ebadd6
hoan.bt <hoan.bt@sutrixsolutions.com>
2023-02-15 10:33:48 +0700

    update new template

--- reader_web/src/Home.js
+++ reader_web/src/Home.js
@@ -1,17 +1,12 @@
 import React, { Component } from "react";
+import Header from "./Header";
  
 class Home extends Component {
   render() {
     return (
         <div>
-          <h1>Simple SPA</h1>
-          <ul className="header">
-            <li><a href="/">Home</a></li>
-            <li><a href="/stuff">Stuff</a></li>
-            <li><a href="/contact">Contact</a></li>
-          </ul>
-          <div className="content">
-             
+          <div class="container">
+             <Header />
           </div>
         </div>
     );
