7adb4e66abe58c6c0220e5237e44382a3e57e65f
hoan.bt <hoan.bt@sutrixsolutions.com>
2023-02-15 12:17:33 +0700

    implement new template

--- reader_web/src/Footer.js
+++ reader_web/src/Footer.js
@@ -157,15 +157,6 @@ class Footer extends Component {
                 </div>
                 </div>
             </section>
-
-            <script>
-                window.onscroll = function() { this.myFunction()};
-
-                var navbar_sticky = document.getElementById("navbar_sticky");
-                var sticky = navbar_sticky.offsetTop;
-                var navbar_height = document.querySelector('.navbar').offsetHeight;
-                
-            </script>
         </div>
     );
   }
