a46c0bbd452057a5ea93403a93de1bed50df4d0f
hoan.bt <hoan.bt@sutrixsolutions.com>
2023-02-15 10:38:16 +0700

    update new template

--- reader_web/src/Header.js
+++ reader_web/src/Header.js
@@ -3,27 +3,93 @@ import React, { Component } from "react";
 class Header extends Component {
   render() {
     return (
-        <header class="tm-site-header">
-            <h1 class="tm-site-name">Shelf</h1>
-            <p class="tm-site-description">Your Online Bookstore</p>
-            
-            <nav class="navbar navbar-expand-md tm-main-nav-container">
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#tmMainNav" aria-controls="tmMainNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <i class="fa fa-bars"></i>
-                </button>
-
-                <div class="collapse navbar-collapse tm-main-nav" id="tmMainNav">
-                    <ul class="nav nav-fill tm-main-nav-ul">
-                        <li class="nav-item"><a class="nav-link active" href="index.html">Home</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#">Catalogs</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#">Awards</a></li>
-                        <li class="nav-item"><a class="nav-link" href="team.html">Our Team</a></li>
-                        <li class="nav-item"><a class="nav-link" href="contact.html">Contact Us</a></li>
-                    </ul>
+        <div>
+            <section id="top">
+                <div class="container">
+                <div class="row top_1">
+                <div class="col-md-3">
+                <div class="top_1l pt-1">
+                    <h3 class="mb-0"><a class="text-white" href="index.html"><i class="fa fa-video-camera col_red me-1"></i> Planet</a></h3>
                 </div>
-            </nav>
-            
-        </header>
+                </div>
+                <div class="col-md-5">
+                <div class="top_1m">
+                    <div class="input-group">
+                                <input type="text" class="form-control bg-black" placeholder="Search Site..." />
+                                <span class="input-group-btn">
+                                    <button class="btn btn text-white bg_red rounded-0 border-0" type="button">
+                                        Search</button>
+                                </span>
+                        </div>
+                </div>
+                </div>
+                <div class="col-md-4">
+                <div class="top_1r text-end">
+                    <ul class="social-network social-circle mb-0">
+                            <li><a href="#" class="icoRss" title="Rss"><i class="fa fa-instagram"></i></a></li>
+                            <li><a href="#" class="icoFacebook" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+                            <li><a href="#" class="icoTwitter" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+                            <li><a href="#" class="icoGoogle" title="Google +"><i class="fa fa-youtube"></i></a></li>
+                            <li><a href="#" class="icoLinkedin" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
+                        </ul>
+                </div>
+                </div>
+                </div>
+                </div>
+            </section>
+
+
+            <section id="header">
+                <nav class="navbar navbar-expand-md navbar-light" id="navbar_sticky">
+                <div class="container">
+                    <a class="navbar-brand text-white fw-bold" href="index.html"><i class="fa fa-video-camera col_red me-1"></i> Planet</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mb-0">
+                        <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="index.html">Home</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="about.html">About Us</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Blog
+                        </a>
+                        <ul class="dropdown-menu drop_1" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="blog.html">Blog</a></li>
+                            <li><a class="dropdown-item border-0" href="blog_detail.html">Blog Detail</a></li>
+                        </ul>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="services.html">Services</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="team.html">Team</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Pages
+                        </a>
+                        <ul class="dropdown-menu drop_1" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="about.html">About Us</a></li>
+                            <li><a class="dropdown-item" href="services.html">Services</a></li>
+                            <li><a class="dropdown-item" href="team.html">Team</a></li>
+                            <li><a class="dropdown-item border-0" href="contact.html">Contact</a></li>
+                        </ul>
+                        </li>
+                        
+                        <li class="nav-item">
+                        <a class="nav-link" href="contact.html">Contact Us</a>
+                        </li>
+                    </ul>
+                    </div>
+                </div>
+                </nav>
+            </section>
+        </div>
     );
   }
 }
