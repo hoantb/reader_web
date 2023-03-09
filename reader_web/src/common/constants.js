f89399bc236b48d726ef89aaee9e4acc7b4072f1
hoan.bt <hoan.bt@sutrixsolutions.com>
2023-03-09 10:52:54 +0700

    change domain

--- reader_web/src/common/constants.js
+++ reader_web/src/common/constants.js
@@ -1,3 +1,3 @@
-export const SCHEME = "https"
-export const HOST = "truyenchoban.com"
+export const SCHEME = "http"
+export const HOST = "ittruyen.com"
 export const API_URL = SCHEME + "://" + HOST;
