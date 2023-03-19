ca2c4fa59c85d0ab17cbebf19d517244b69af39c
hoan.bt <hoan.bt@sutrixsolutions.com>
2023-03-19 22:51:57 +0700

    fix reading on phone

--- reader_web/src/PdfPreview.js
+++ reader_web/src/PdfPreview.js
@@ -6,6 +6,7 @@ import { Document, Page,pdfjs } from 'react-pdf';
 import Center from './Center'
 import Header from "./Header";
 import './PdfPreview.css'
+import Footer from "./Footer";
 
 // const styles = StyleSheet.create({
 //   page: { backgroundColor: 'tomato' },
@@ -25,7 +26,8 @@ class PdfPreview extends Component {
             pageNumber: 1,
             numPages: 10,
             render_anotate: false,
-            isLoaded:false
+            isLoaded:false,
+            width: 500
         };
         this.document_ref = React.createRef()
         this.onDocumentLoadSuccess = this.onDocumentLoadSuccess.bind(this);
@@ -58,7 +60,8 @@ class PdfPreview extends Component {
   }
 
   componentDidMount() {
-    
+      //this.state.width = window.innerWidth;
+      console.log(window.innerWidth)
       pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
       console.log(this.props.params)
       fetch( ConstantsVar.API_URL + "/api/files" + "/" + this.props.params.id)
@@ -66,7 +69,7 @@ class PdfPreview extends Component {
       .then(
           (result) => {
               console.log(result);
-              this.setState({file: result})
+              this.setState({file: result, width: window.innerWidth})
           }
       )
       // console.log(this.document_ref.current.classList)
@@ -78,26 +81,28 @@ class PdfPreview extends Component {
         <Header />
           <section id="center" className="center_home">
               <div className="carousel-inner">
-                  {this.state.file &&
-                      <Document className="centerPdf" loading="Đang tải truyện vui lòng chờ..." file={this.state.file.file} onLoadSuccess={ this.onDocumentLoadSuccess }>
-                          <Page className="centerPdf" pageNumber={this.state.pageNumber} renderAnnotationLayer={this.state.render_anotate} renderForms={this.state.render_anotate}/>
-                      </Document>
-
-                  }
-                  { this.state.isLoaded &&
-                    <div className="centerButtonPdf">
-                        Page {this.state.pageNumber || (this.state.numPages ? 1 : '--')} of { this.state.numPages || '--'}
-                        <ul >
-                            <li className="d-inline-block me-2">
-                                  <button className="btn btn text-white bg_red rounded-0 border-0" type="button" onClick={this.previousPage}> Trang trước</button>
-                            </li>
-                            <li className="d-inline-block">
-                                  <button className="btn btn text-white bg_red rounded-0 border-0" type="button" onClick={this.nextPage}> Trang kế tiếp</button>
-                            </li>
-                        </ul>
+                      {this.state.file &&
+                        <div className="d-flex align-items-center justify-content-center col-md-12">
+                            <Document className="centerPdf" loading="Đang tải truyện vui lòng chờ..." file={this.state.file.file} onLoadSuccess={ this.onDocumentLoadSuccess }>
+                                <Page className="centerPdf" width={this.state.width} pageNumber={this.state.pageNumber} renderAnnotationLayer={this.state.render_anotate} renderForms={this.state.render_anotate} renderTextLayer={this.state.render_anotate}/>
+                            </Document>
+                        </div>
+                      }
+                      { this.state.isLoaded &&
+                        <div className="centerButtonPdf">
+                            Page {this.state.pageNumber || (this.state.numPages ? 1 : '--')} of { this.state.numPages || '--'}
+                            <ul >
+                                <li className="d-inline-block me-2">
+                                      <button className="btn btn text-white bg_red rounded-0 border-0" type="button" onClick={this.previousPage}> Trang trước</button>
+                                </li>
+                                <li className="d-inline-block">
+                                      <button className="btn btn text-white bg_red rounded-0 border-0" type="button" onClick={this.nextPage}> Trang kế tiếp</button>
+                                </li>
+                            </ul>
+                      </div>
+                  
+                      }
                   </div>
-                }
-              </div>
             </section>
       </div>
     )
