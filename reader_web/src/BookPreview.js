0495b6a627d570a85c1586688372147eaa640485
hoan.bt <hoan.bt@sutrixsolutions.com>
2023-03-19 18:25:53 +0700

    update book preview

--- reader_web/src/BookPreview.js
+++ reader_web/src/BookPreview.js
@@ -11,7 +11,8 @@ class BookPreview extends Component {
         super(props);
 
         this.state = {
-            book: {}
+            book: {},
+            comments: []
         }
     }
 
@@ -21,7 +22,7 @@ class BookPreview extends Component {
         .then(
             (result) => {
                 console.log(result);
-                this.setState({book: result})
+                this.setState({book: result, comments: result.comments})
             }
         )
     }
@@ -73,65 +74,50 @@ class BookPreview extends Component {
                                         </div>
                                         <div className="blog_1l1i mt-3">
                                             <h5><i className="fa fa-folder-open col_red me-1"></i> Giới thiệu truyện</h5>
-                                            {this.state.book.image_preview &&
-                                                <h2 className="mt-3"><a className="col_red" href="#"></a>{this.state.book.title}</h2>
+                                            {this.state.book &&
+                                                <div>
+                                                    <h2 className="mt-3"><a className="col_red" href="#"></a>{this.state.book.title}</h2>
+                                                    <h6 className="fw-normal mt-3 col_light">
+                                                        <span><i className="fa fa-clock-o me-1 align-middle col_red"></i> 14 December 2021</span>
+                                                        <span className="ms-3"><i className="fa fa-user me-1 align-middle col_red"></i> {this.state.book.author_name}</span>
+                                                    </h6>
+                                                    <p className="mt-3">{this.state.book.description}</p>
+                                                </div>
                                             }
-                                            <h6 className="fw-normal mt-3 col_light">
-                                            <span><i className="fa fa-clock-o me-1 align-middle col_red"></i> 14 December 2021</span>
-                                            <span className="ms-3"><i className="fa fa-user me-1 align-middle col_red"></i> Admin</span>
-                                            <span className="ms-3"><i className="fa fa-comment me-1 align-middle col_red"></i> Comment</span>
-                                            </h6>
-                                            <p className="mt-3">Nhà phát triển đại học Consectetuer có đường kính, Offset Euismod Estat tincidalal đến LAOREET một khoản tiền lớn vào cuối tuần. Để sôi sục cho Minimatisa Come, người đã tập thể dục Tation Ullamcorper Suscipit Carton Player. Tên sách được coi là không tải tùy chọn tùy chọn không có phần</p>
                                         </div>
                                     </div>
-                                    <div className="blog_1l2">
-                                        <p>Bài tập về nhà hoặc anh ta là người tiêu dùng ở Cục trong một công thức công thức nhân viên ngoài trời hoặc đau đớn Feugiat Nulla Nulla Chất chống oxy hóa nhưng đậu phộng và lớp và thường xuyên. Bản thân Lorem chỉ đơn giản là văn bản giả của ngành in và sắp chữ. Lorem là văn bản tiêu chuẩn của ngành kể từ những năm 1500, khi một máy in không xác định lấy một loại galley và tranh giành nó để tạo ra một mẫu vật mà nó đã phát triển không chỉ năm thế kỷ, mà còn là bước nhảy vọt vào việc sắp xếp điện tử.</p>
-                                        <blockquote className="blockquote bg_dark p-4 pt-3 mt-4">
-                                                                        <p>Provident tránh các mùa, kiến ​​trúc sư của đa số cuộc gặp gỡ lớn hơn khi được làm hài lòng kiến ​​trúc sư Auny Koi Thako, và lời buộc tội nhỏ nhất trong sự cản trở của họ và</p>
-                                                                        <h6 className="fw-normal mb-0"><strong>- Dapibus Diam </strong>of Google Inc.</h6>
-                                                                    </blockquote>
-                                        <p>Nó đã được phổ biến vào những năm 1980 với việc phát hành các tờ Letraset chứa Lorem rất nhiều đoạn, và gần đây hơn với phần mềm xuất bản máy tính để bàn như Aldus Pagemaker bao gồm cả văn bản rất quan trọng. Lorem rất cà rốt, nhà phát triển đại học cà chua, nhưng tôi sẽ ở trong một bản nhạc, và nỗi đau của một thời gian tuyệt vời. Ví dụ, với sự ngạc nhiên, ai đã thực hiện bất kỳ việc thực hiện việc thực hiện bất kỳ việc thực hiện việc làm nào, nhưng như là sự tiện lợi của sự thuận tiện. Bài tập về nhà hoặc một nỗi đau trong niềm vui bị chỉ trích là một cặp thoát khỏi nỗi đau. Ngoại trừ họ là những người rèm háo hức không làm nguyên tắc, họ là những món ăn nhẹ iest.</p>
-                                        <ul>
-                                            <li className="d-inline-block me-3"><a href="#"><i className="fa fa-tag me-1 col_red"></i> Blog</a></li>
-                                            <li className="d-inline-block me-3"><a href="#"><i className="fa fa-tag me-1 col_red"></i> Creative</a></li>
-                                            <li className="d-inline-block me-3"><a href="#"><i className="fa fa-tag me-1 col_red"></i> Business</a></li>
-                                            <li className="d-inline-block"><a href="#"><i className="fa fa-tag me-1 col_red"></i> News</a></li>
-                                        </ul>	
-                                        <ul className="social-network social-circle mb-0 mt-3">
-                                            <li><a href="#" className="icoFacebook" title="Facebook"><i className="fa fa-facebook"></i></a></li>
-                                            <li><a href="#" className="icoTwitter" title="Twitter"><i className="fa fa-twitter"></i></a></li>
-                                            <li><a href="#" className="icoGoogle" title="Google +"><i className="fa fa-instagram"></i></a></li>
-                                            <li><a href="#" className="icoLinkedin" title="Linkedin"><i className="fa fa-linkedin"></i></a></li>
-                                        </ul>					
-                                    </div>
-                                    <div className="blog_1l3 mt-4">
-                                        <h3>Bình luận gần đây</h3>
-                                    </div>
-                                    <div className="blog_1l5 mt-3">
-                                        {
-                                            this.state.book.comments &&
-                                            this.state.book.comments.map(
-                                                comment => (
-                                                    <div key={"comment_" + comment.id}>
-                                                    <div className="blog_1l5i row">
-                                                        <div className="col-md-2 col-2 pe-0">
-                                                        <div className="blog_1l5il">
-                                                        <img src={comment.visitor.avatar} className="w-100" alt="abc" />
-                                                        </div>
-                                                        </div>
-                                                        <div className="col-md-10 col-10">
-                                                            <div className="blog_1l5ir">
-                                                                <h5><a href="#">{comment.visitor.name}</a> <span className="font_14 col_light">/ 13 July 2017</span></h5>
-                                                                <p className="font_14">{comment.comment}</p>
-                                                            </div> 
-                                                        </div>
-                                                    </div>
-                                                    <hr />
-                                                    </div>
-                                                )
-                                            )
-                                        }
-                                    </div>
+                                    {
+                                        this.state.comments.length > 0 &&
+                                        <div>
+                                            <div className="blog_1l3 mt-4">
+                                                <h3>Bình luận gần đây</h3>
+                                            </div>
+                                            <div className="blog_1l5 mt-3">
+                                                {
+                                                    this.state.comments.map(
+                                                        comment => (
+                                                            <div key={"comment_" + comment.id}>
+                                                            <div className="blog_1l5i row">
+                                                                <div className="col-md-2 col-2 pe-0">
+                                                                <div className="blog_1l5il">
+                                                                <img src={comment.visitor.avatar} className="w-100" alt="abc" />
+                                                                </div>
+                                                                </div>
+                                                                <div className="col-md-10 col-10">
+                                                                    <div className="blog_1l5ir">
+                                                                        <h5><a href="#">{comment.visitor.name}</a> <span className="font_14 col_light">/ 13 July 2017</span></h5>
+                                                                        <p className="font_14">{comment.comment}</p>
+                                                                    </div> 
+                                                                </div>
+                                                            </div>
+                                                            <hr />
+                                                            </div>
+                                                        )
+                                                    )
+                                                }
+                                            </div>
+                                        </div>
+                                    }
                                     <div className="blog_1l3 mt-4">
                                     <h3>Bình luận</h3>
                                     </div>
